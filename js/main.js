20578349b288dba3195ce49b983096602c17b2f8
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-04-18 16:00:03 +0200

    esercizio nel dom

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
 let emailList = ["mail1@email.com", "mail2@email.com", "mail3@email.com", "mail4@email.com",];
 
-let emailToCheck = prompt("Inserisci la tua mail");
-let Check = false;
+let result = document.getElementById("result");
 
+function checkEmail(){
 
-for(let i = 0 ; i < emailList.length ; i++){
+    let emailToCheck = document.getElementById("mailInput").value;
 
-    let currentMail = emailList[i];
+    console.log(emailToCheck);
+    let Check = false;
 
-    if(emailToCheck == currentMail){
-        Check = true;
+    for(let i = 0 ; i < emailList.length ; i++){
+
+        let currentMail = emailList[i];
+    
+        if(emailToCheck == currentMail){
+            Check = true;
+        }
+    }
+    
+    if(Check){
+        result.innerText = "SEI AUTORIZZATO"
+    }else{
+        result.innerText = "NON SEI AUTORIZZATO";
     }
 }
 
-if(Check){
-    alert("Sei autorizzato!");
-}else{
-    alert("NON sei autorizzato!");
-}
